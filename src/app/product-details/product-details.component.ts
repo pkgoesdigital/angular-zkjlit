4cffaacb3a61a1e37b04509dab52150736fd9de7
pkgoesdigital <klimaspaula@gmail.com>
2019-12-05 13:50:40 -0700

    3 files modified, 1 added, adds buy button and cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,11 +11,15 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-
-// inject ActivatedRoute into constructor
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
+// inject ActivatedRoute into constructor and CartService
 // is specific to each routed component loaded by Angular Router 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
