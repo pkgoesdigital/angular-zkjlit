109ba72ae4eb9f817d91d8bbc835398f56e2b2e9
pkgoesdigital <klimaspaula@gmail.com>
2019-12-05 13:33:00 -0700

    2 files modified, 3 added, routing integration

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+// inject ActivatedRoute into constructor
+// is specific to each routed component loaded by Angular Router 
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
+  }
+
+}
